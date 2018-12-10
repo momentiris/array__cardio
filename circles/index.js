afc3a01c9b4426d7f51d13584d0fb02aa520465f
momentiris <momentiris@gmail.com>
2018-12-10 23:23:51 +0100

    rdy

---
+++ circles/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// 1. Fetch url https://arraycardio.herokuapp.com/circles
+// 2. loop over the circles data and create html template tags <div class="circle"></div>.
+//    Assign it a css background property that you get from the fetched data.
+// 3. Insert into document.body
